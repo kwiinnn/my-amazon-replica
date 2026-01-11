730a173a7f322173170797cc64a904b60b12b569
kwiinnn <solomon23charles@gmail.com>
2026-01-11 19:17:53 +0800

    Made 'cart quantity' interactive

--- javascript-amazon-project/scripts/amazon.js
+++ javascript-amazon-project/scripts/amazon.js
@@ -79,6 +79,13 @@ document.querySelectorAll('.js-add-to-cart')
         });
       }
 
+      let cartQuantity = 0;
+      cart.forEach((item) => {
+        cartQuantity += item.quantity;
+      });
+
+      document.querySelector('.js-cart-quantity').innerHTML = cartQuantity;
+
 			console.log(cart);
 		});
 	});
