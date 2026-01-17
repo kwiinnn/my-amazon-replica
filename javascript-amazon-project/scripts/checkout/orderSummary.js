e910d10d3041913e902cadc7a17115c871c12883
kwiinnn <solomon23charles@gmail.com>
2026-01-17 22:06:30 +0800

    Made payment summary interactive

--- javascript-amazon-project/scripts/checkout/orderSummary.js
+++ javascript-amazon-project/scripts/checkout/orderSummary.js
@@ -3,6 +3,7 @@ import {products, getProduct} from '../../data/products.js';
 import {formatCurrency} from '../utils/money.js';
 import dayjs from 'https://unpkg.com/supersimpledev@8.5.0/dayjs/esm/index.js';
 import {deliveryOptions, getDeliveryOption} from '../../data/deliveryoptions.js';
+import { renderPaymentSummary } from './paymentSummary.js';
 
 export function renderOrderSummary() {
 
@@ -111,6 +112,8 @@ export function renderOrderSummary() {
 					`.js-cart-item-container-${productId}`
 				);
 				container.remove();
+
+				renderPaymentSummary();
 			});
 		});
 
@@ -120,6 +123,7 @@ export function renderOrderSummary() {
 				const {productId, deliveryOptionId} = element.dataset;
 				updateDeliveryOption(productId, deliveryOptionId);
 				renderOrderSummary();
+				renderPaymentSummary();
 			});
 		});
 }
