5a7510989e5c8fbdfe92501763116c8e5e007d1c
kwiinnn <solomon23charles@gmail.com>
2026-01-21 00:27:17 +0800

    Renamed test-jasmine folder to tests, and organized folders

--- javascript-amazon-project/tests/moneyTests.js
+++
@@ -1,3 +0,0 @@
-import { formatCurrency } from "../scripts/utils/money.js";
-
-console.log(formatCurrency(2000.5))
---
+++ javascript-amazon-project/tests/test-simple/moneyTests.js
@@ -0,0 +1,3 @@
+import { formatCurrency } from "../../scripts/utils/money.js";
+
+console.log(formatCurrency(2000.5))
