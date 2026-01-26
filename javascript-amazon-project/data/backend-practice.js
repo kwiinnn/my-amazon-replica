b14e434441eec67bc489b589de023a84edb5c934
kwiinnn <solomon23charles@gmail.com>
2026-01-26 22:38:14 +0800

    Used backend to load files in product.js

--- javascript-amazon-project/data/backend-practice.js
+++ javascript-amazon-project/data/backend-practice.js
@@ -4,6 +4,6 @@ xhr.addEventListener('load', () => {
 	console.log(xhr.response);
 });
 
-xhr.open('GET', 'https://supersimplebackend.dev/products/first');
+xhr.open('GET', 'https://supersimplebackend.dev/hello');
 xhr.send();
 xhr.response
--- javascript-amazon-project/data/products.js
+++ javascript-amazon-project/data/products.js
@@ -54,6 +54,28 @@ class Clothing extends Product {
   
 }
 
+export let products = [];
+
+function loadProducts() {
+  const xhr = new XMLHttpRequest();
+
+  xhr.addEventListener('load', () => {
+    products = JSON.parse(xhr.response).map((productDetails) => {
+  if (productDetails.type === 'clothing') {
+    return new Clothing(productDetails);
+  }
+  return new Product(productDetails);
+    });
+    console.log('load products');
+  });
+  xhr.open('GET', 'https://supersimplebackend.dev/products');
+  xhr.send();
+}
+
+loadProducts();
+
+
+/*
 export const products = [
   {
     id: "e43638ce-6aa0-4b85-b27f-e1d07eb678c6",
@@ -719,3 +741,4 @@ export const products = [
   }
   return new Product(productDetails);
 });
+*/
